5bb2f45fe3679876c99ec9c8da93723113e6e3d2
stevexn <stevexn@gmail.com>
2021-02-19 19:05:13 -0400

    d

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ def handle(msg):
             comp=command.split()
             c=int(comp[1])
             if c in reg:
-                bot.sendMessage(chat_id,"Usted es el numero")
+                bot.sendMessage(chat_id,"Usted es el numero "+ c)
             else:
                 bot.sendMessage(chat_id, "Usted no esta registrado")
         except(TypeError, NameError, ValueError):
