2d99b8752c7552f904e7e202653fb0746d65b23f
stevexnx <39393462+stevexnx@users.noreply.github.com>
2021-04-09 22:01:31 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,99 +1,92 @@
+import datetime
+import telepot
 import os
-import qrcode
-from telegram.ext import CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, Filters
-from telegram import ChatAction, InlineKeyboardMarkup, InlineKeyboardButton
+import time
+import sys
 
-INPUT_TEXT = 0
+global reg
+reg=[]
 
-def start(update, context):
+def handle(msg):
+    #registros=[]
+    chat_id = msg['chat']['id']
+    command = msg['text']
 
-    update.message.reply_text(
-        text = "Hola, Bienvenido, ¿Que desea hacer?\n\n Utiliza /qr para generar un Código qr.",
-        
-        reply_markup = InlineKeyboardMarkup([
-            [InlineKeyboardButton(text = "Generar código qr", callback_data = "qr")],
-            [InlineKeyboardButton(text = "Sobre los autores", url = "https://www.googl.com.do")]
-        ])
-    )
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    def start(command):
+        try:
+            bot.sendMessage(chat_id,("Bienvenido"))
+            bot.sendMessage(chat_id,("Puedes utilizar los siguientes comandos: \n"))
+            bot.sendMessage(chat_id,("1) /ingresar + (tu matrícula). Para agregarte a la cola."))
+            bot.sendMessage(chat_id,("2) /consultar + (tu matrícula). Para consultar tu numero de ticket."))
+            bot.sendMessage(chat_id,("3) /borrar + (tu matrícula). Para eliminarte de la lista."))
+        except(TypeError, NameError, ValueError):
+            bot.sendMessage(chat_id,"Ha habido algún error. \nCodigo #S1. Si persiste, comuniquese con el administrador.")
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    def Ingresar_Articulo(command):
+        try:
+            datos=command.split()
+            placa=int(datos[1])
+            if len(reg)<10:
+                reg.append(placa)
+                bot.sendMessage(chat_id, ("Usted a sido añadido satisfactoriamente."))
+                bot.sendMessage(chat_id, reg)
+            else:
+                bot.sendMessage(chat_id ,"No quedan asientos.")
+        except(TypeError, NameError, ValueError):
+            bot.sendMessage(chat_id,"Ha habido algún error. \nCodigo #I1. Si persiste, comuniquese con el administrador.")
 
-def qr_command_handler(update, context):
-    
-    update.message.reply_text("¿De que quieres el código QR?")
-    
-    return INPUT_TEXT
-
-def qr_callback_handler(update, context):
-   
-   query = update.callback_query
-   query.answer()
-
-   query.edit_message_text(
-       text = "¿De que quieres el código QR?"
-   )
-
-   return INPUT_TEXT
-
-def generate_qr(text):
-
-    filename = text + ".jpg"
-    
-    img = qrcode.make(text)
-    
-    img.save(filename)
-
-    return filename
-
-def sed_qr(filename, chat):
-
-    chat.send.action(
-        action = ChatAction.upload_photo,
-        timeout = None
-    )
-
-    chat.send_photo(
-        photo = open(filename, 'rb')
-    )
-
-    os.unlink(filename)
-
-def input_text(update, context):
-    
-    text =  update.message.text
-    
-    filename = generate_qr(text)
-    
-    chat = update.message.chat
-    
-    send_qr(filename, chat)
-
-    return ConversationHandler.END
-
-if __name__ == "__main__":
-    
-    updater = Updater(token = "1506443426:AAHW6mxxM18pBi85Wpd5sgUPFmFB6QCHo-M", use_context = True) 
-
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(ConversationHandler(
-        entry_points = [
-            CommandHandler("qr", qr_command_handler),
-            CallbackQueryHandler(pattern = "qr", callback = qr_callback_handler)
-        ],
-
-        states = {
-            INPUT_TEXT: [MessageHandler(Filters.text, input_text)]
-        },
-
-        fallbaks = []        
-    ))
-
-    updater.start_polling()
-    updater.idle()
-
-
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    def Consultar_Articulo(command):
+        try:
+            comp=command.split()
+            c=int(comp[1])
+            if c in reg:
+                bot.sendMessage(chat_id,"Usted ya esta registrado.")
+            else:
+                bot.sendMessage(chat_id, "Usted aún no esta registrado.")
+        except(TypeError, NameError, ValueError):
+            bot.sendMessage(chat_id,"Ha habido algún error. \nCodigo #C1. Si persiste, comuniquese con el administrador.")
+            
+    #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    def Borrar(command):#funcion para borrar un articulo
+        try:
+            comp=command.split()
+            c=int(comp[1])
+            if c in reg:
+                reg.remove(c)
+                bot.sendMessage(chat_id,"Usted ha sido eliminado satisfactoriamente.")
+            else:
+                bot.sendMessage(chat_id, "Usted no esta registrado.")
+        except(TypeError, NameError, ValueError):
+            bot.sendMessage(chat_id,"Ha habido algún error. \nCodigo #B1. Si persiste, comuniquese con el administrador.")
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    lista1=['start','/ingresar','/consultar','/borrar']
+    div=command.split()
+    comparacion = []
+    for item in lista1:
+        if item in div:
+            comparacion.append(item)
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    try:
+        if command==("/start"):#Para inicar el bot#
+            start(command)#la funcion tiene los valores de entrada del id de la persona y el objeto bot, para no utilizaro 2 veces#
+        elif comparacion[0]==("/ingresar"):#Para comprar un ticket#
+            Ingresar_Articulo(command)#Solo es posible infresar caracteres numericos#
+        elif comparacion[0]==("/consultar"):#Para consultar si has comprado un ticket#
+            Consultar_Articulo(command)#Se debe ingresar la matricula.
+        elif comparacion[0]==("/borrar"):#Para borrar tu compra#
+            Borrar(command)#Se debe ingresar la matricula que se quiere borrar#
+    except(IndexError):
+        bot.sendMessage(chat_id, ("Ha habido algún error. \nCodigo. #CM.Error. Si persiste, comuniquese con el administrador."))
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
 
 
 TOKEN=("1506443426:AAHW6mxxM18pBi85Wpd5sgUPFmFB6QCHo-M")
 bot = telepot.Bot(TOKEN)
 bot.message_loop(handle)
 print ('Estoy escuchando...')
+
+
+while 1:
+     time.sleep(10)
